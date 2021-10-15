fd66dae0b0e0c98809fc925f110c312b1e521a68
Simon Montaño <simon_montano@yahoo.com>
2021-10-14 21:21:30 -0400

    Add files via upload

---
+++ includes/javascripts.php
@@ -0,0 +1,16 @@
+  <!-- Bootstrap core JavaScript-->
+  <script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
+  <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script type="text/javascript" src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script type="text/javascript" src="assets/js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script type="text/javascript" src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script type="text/javascript" src="assets/js/demo/chart-area-demo.js"></script>
+  <script type="text/javascript" src="assets/js/demo/chart-pie-demo.js"></script>
